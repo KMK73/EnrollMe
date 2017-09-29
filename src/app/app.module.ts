d356cd49d17caaf9a9ac7f258afcd6cbccdbf6ad
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-09-29 13:02:06 -0400

    start of profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,48 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { StudentsComponent } from './pages/students/students.component';
+import { ProfileBarComponent } from './components/profile-bar/profile-bar.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
 
+import { BootstrapModalModule } from 'ng2-bootstrap-modal';
+import { AlertModule, BsModalService } from 'ngx-bootstrap';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { MdButtonModule, MdCardModule, MdMenuModule, MdToolbarModule, MdIconModule, MdSelectModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'user', component: ProfileComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    StudentsComponent
+    StudentsComponent,
+    ProfileBarComponent,
+    ProfileComponent,
+    AddStudentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdCardModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdSelectModule,
+    BootstrapModalModule,
+    BootstrapModalModule.forRoot({container: document.body}),
+    AlertModule.forRoot(),
+  ],
+  entryComponents: [
+    AddStudentComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Router } from '@angular/router';
+// import { BsModalService } from 'ngx-bootstrap';
+// import { BsModalRef } from 'ngx-bootstrap/modal/modal-options.class';
+import { AddStudentComponent } from '../../components/add-student/add-student.component';
+import { DialogService } from 'ng2-bootstrap-modal';
+import { MdSelect } from '@angular/material';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  // public modalRef: BsModalRef;
 
-  constructor() { }
+  constructor(private router: Router, private dialogService: DialogService ) { 
+
+  }
+
+  selectedValue: string;
+  
+  grades = [
+    {value: '0', viewValue: 'Grade 1'},
+    {value: '1', viewValue: 'Grade 2'},
+    {value: '2', viewValue: 'Grade 3'},
+    {value: '3', viewValue: 'Grade 4'}
+  ];
 
   ngOnInit() {
   }
 
+  viewProfile() {
+    let link = ['/user'];
+    this.router.navigate(link);
+  }
+
+  showConfirm() {
+    this.dialogService.addDialog(AddStudentComponent, {title: 'Add a Student', message: 'Alert message!!!'});
+    
+    // let disposable = this.dialogService.addDialog(AddStudentComponent, {
+    //     title:'Confirm title', 
+    //     message:'Confirm message'})
+    //     .subscribe((isConfirmed) => {
+    //         // We get dialog result
+    //         if (isConfirmed) {
+    //             alert('accepted');
+    //         } else {
+    //             alert('declined');
+    //         }
+    //     });
+    // // We can close dialog calling disposable.unsubscribe();
+    // // If dialog was not closed manually close it by timeout
+    // setTimeout(() => {
+    //     disposable.unsubscribe();
+    // }, 10000);
+}
 }
