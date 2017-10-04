1a95defbc1e236baf409fbe97e28c72c5988c1a4
Kelsey Kjeldsen <kelsey.kjeldsen@gmail.com>
2017-10-03 20:51:26 -0400

    initial heroku

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express'); 
-const http = require('http'); 
+// const http = require('http'); 
 const path = require('path'); 
 
 // const api = require('./server/routes/api'); 
 
 const app = express(); 
 
-app.use(express.static(path.join(__dirname, 'dist'))); 
+app.use(express.static(__dirname, '/dist')); 
 
+
+app.listen(process.env.PORT || 8080); 
+
+// const port = process.env.PORT ||'3001'; 
+// app.set('port', port);
+
+// PathLocationStrategy 
+// angular to handle routing not server
 app.get('*', (req, res)=> {
     res.sendfile(path.join(__dirname, 'dist/index.html')); 
 });
 
-const port = process.env.PORT ||'3001'; 
-app.set('port', port);
-
-const server = http.createServer(app); 
-server.listen(port, ()=> console.log('Running')); 
+console.log('console listening');
+// const server = http.createServer(app); 
+// server.listen(port, ()=> console.log('Running')); 
